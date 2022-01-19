005c40f8e921a3acf2461abad94fd5d3e2dea340
Maxi Peszano <maximiliano@upshow.tv>
2022-01-18 23:42:41 -0300

    Add mongoDB connection and error handler.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db.js');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -16,4 +19,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+})
