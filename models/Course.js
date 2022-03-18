31d2d4e812e1f5804fcece821e5e349f6ad63c62
maxpsz <maximiliano@upshow.tv>
2022-03-18 09:11:02 -0300

    Add course model and seeder.

---
+++ models/Course.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a course title']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description']
+    }
+})
--- seeder.js
+++ seeder.js
@@ -6,14 +6,17 @@ require('colors');
 dotenv.config({ path: './config/config.env' });
 
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 mongoose.connect(process.env.MONGO_URI);
 
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Bootcamp.create(courses);
 
         console.log('Data Imported...'.green.inverse);
         process.exit();
@@ -25,6 +28,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
 
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
