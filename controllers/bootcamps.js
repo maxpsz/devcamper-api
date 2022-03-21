9e9e93f8dde254efb04518146bbc819894db1250
maxpsz <maximiliano@upshow.tv>
2022-03-21 01:26:46 -0300

    Change bootcamps controller exports.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,7 +6,7 @@ const geocoder = require('../utils/geocoder');
 //@desc     Get all bootcamps
 //@route    GET /api/v1/bootcamps
 //@access   Public
-exports.getBootcamps = asyncHandler(async (req, res, next) => {
+const getBootcamps = asyncHandler(async (req, res, next) => {
     const reqQuery = { ...req.query };
 
     let query;
@@ -61,7 +61,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 //@desc     Get single bootcamp
 //@route    GET /api/v1/bootcamps/:id
 //@access   Public
-exports.getBootcamp = asyncHandler(async (req, res, next) => {
+const getBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     res.status(200).json({ success: true, data: bootcamp });
@@ -70,7 +70,7 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 //@desc     Create new bootcamp
 //@route    POST /api/v1/bootcamps
 //@access   Private
-exports.createBootcamp = asyncHandler(async (req, res, next) => {
+const createBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.create(req.body);
     res.status(200).json({ success: true, data: bootcamp });
 });
@@ -78,7 +78,7 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 //@desc     Update bootcamp
 //@route    PUT /api/v1/bootcamps/:id
 //@access   Private
-exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+const updateBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true
@@ -90,7 +90,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 //@desc     Delete bootcamp
 //@route    DELETE /api/v1/bootcamps/:id
 //@access   Private
-exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+const deleteBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
@@ -103,7 +103,7 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 //@desc     Get bootcamps by radius
 //@route    GET /api/v1/bootcamps/radius/:zipcode/:distance
 //@access   Public
-exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+const getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     const { zipcode, distance } = req.params;
 
     const [loc] = await geocoder.geocode(zipcode);
@@ -118,3 +118,12 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
 });
+
+module.exports = {
+    getBootcamps,
+    getBootcamp,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp,
+    getBootcampsInRadius
+}
