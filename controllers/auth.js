f3e82a955c9f095014292d42152454279441351c
maxpsz <maximiliano@upshow.tv>
2022-03-26 13:30:35 -0300

    Add User Model

---
+++ controllers/auth.js
@@ -0,0 +1,16 @@
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+
+//@desc     Register user
+//@route    GET /api/v1/auth/register
+//@access   Public
+const register = asyncHandler(async (req, res, next) => {
+    res.status(200).json({
+        success: true
+    });
+});
+
+module.exports = {
+    register
+};
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -20,9 +20,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/')
-.get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-.post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
