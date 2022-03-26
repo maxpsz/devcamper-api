a4e1898d80df00ee06902701881778013cf7ff6b
maxpsz <maximiliano@upshow.tv>
2022-03-26 14:05:33 -0300

    Add User Registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,9 +6,11 @@ const asyncHandler = require('../middleware/async');
 //@route    GET /api/v1/auth/register
 //@access   Public
 const register = asyncHandler(async (req, res, next) => {
-    res.status(200).json({
-        success: true
-    });
+    const { name, email, password, role } = req.body;
+
+    const user = await User.create({ name, email, password, role });
+
+    res.status(200).json({ success: true });
 });
 
 module.exports = {
