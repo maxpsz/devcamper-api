c4a1d57d0b8bc45b3e09c09c3ba00fea7bda35b0
maxpsz <maximiliano@upshow.tv>
2022-04-11 21:14:32 -0300

    Add Bootcamp Review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { getReviews, getReview } = require('../controllers/reviews');
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
 const Review = require('../models/Review');
 const { protect, authorize } = require('../middleware/auth');
 const advancedResults = require('../middleware/advancedResults');
@@ -9,7 +9,8 @@ const router = express.Router({ mergeParams: true });
 
 router
     .route('/')
-    .get(advancedResults(Review, { path: 'bootcamp', select: 'name description' }), getReviews);
+    .get(advancedResults(Review, { path: 'bootcamp', select: 'name description' }), getReviews)
+    .post(protect, authorize('user', 'admin'), addReview);
 
 router
     .route('/:id')
